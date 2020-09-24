6a7b35681723ff5461360b7481826ff075b38803
Buddhika Dananjaya <buddhiextuse@gmail.com>
2020-09-25 01:42:37 +0530

    simple change

--- password_genrater.py
+++ password_genrater.py
@@ -5,10 +5,10 @@ password = " "
 
 for _ in range(3):
     for _ in range(3):
-        A1 = chr(rd.randrange(32,48))#symbols
-        A2 = chr(rd.randrange(48,58))#numbers
-        A3 = chr(rd.randrange(65,90))#Upercase
-        A4 = chr(rd.randrange(97,123))#lowercase
+        A1 = chr(rd.randrange(48,58))#symbols
+        A2 = chr(rd.randrange(58,65))#numbers
+        A3 = chr(rd.randrange(90,97))#Upercase
+        A4 = chr(rd.randrange(97,125))#lowercase
         types = [A1,A2,A3,A4]
         password = password + types[rd.randrange(0,4)]
 
