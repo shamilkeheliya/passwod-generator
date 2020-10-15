6846ab2ab7e5bbe37b442df8fe7207cbf120a290
Senya Tennakoon <72939057+senyatennakoon@users.noreply.github.com>
2020-10-15 23:25:41 +0530

    Update password_genrater.py

--- password_genrater.py
+++ password_genrater.py
@@ -9,15 +9,11 @@ for _ in range(3):
         A2 = chr(rd.randrange(58,65))#numbers
         A3 = chr(rd.randrange(90,97))#Upercase
         A4 = chr(rd.randrange(97,125))#lowercas
-        A5 = chr(rd.randrange(58,65))#numbers
-        A6 = chr(rd.randrange(97,125))#lowercas
-        A7 = chr(rd.randrange(48,58))#symbols
-        A8 = chr(rd.randrange(90,97))#Upercase
 
-        types = [A1,A2,A3,A4,A5,A6,A7,A8]
-        password = password + types[rd.randrange(0,8)]
+        types = [A1,A2,A3,A4]
+        password = password + types[rd.randrange(0,4)]
 
-    password = password + types[rd.randrange(0,8)]
+    password = password + types[rd.randrange(0,4)]
 
 
 print(password)
